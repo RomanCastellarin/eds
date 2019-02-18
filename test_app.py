655a59cdd827c677d7c0f93b992fffd1721bed2f
Varun Gowtham <v.gowtham@tu-berlin.de>
2019-02-18 17:57:10 +0100

    add initial ems tests

---
+++ test_app.py
@@ -0,0 +1,43 @@
+import requests
+import time
+import sys
+import os
+
+ems = os.environ["ET_EMS_LSBEATS_HOST"]
+headers = {'content-type': 'application/json'}
+
+stampers = '''
+when e.strcmp(type,"net") do #NetData
+when e.path(message) do #TJobMsg
+
+when e.tag(#testresulted) do #websocket
+'''
+
+moms = '''
+stream bool testcorrect = e.strmatch(message, "Hello Varun")
+
+trigger tjobfinished do emit testcorrect on #testresulted
+trigger tjobfinished do emit testcorrect on #websocket
+'''
+
+url = "http://" + ems + ":8888/stamper/tag0.1"
+response = requests.post(url, headers=headers, data=stampers)
+print(response.content)
+
+url = "http://" + ems + ":8888/MonitoringMachine/signals0.1"
+response = requests.post(url, headers=headers, data=moms)
+print(response.content)
+
+import websocket import create_connection
+url = "ws://" + ems + ":3232"
+ws = create_connection(url)
+i = 0
+while True:
+  result = ws.recv()
+  print result
+  i+=1
+  if i == 5:
+    print "Hello Varun"
+    break
+
+print "Hello Varun"
