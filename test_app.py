9d4b7b0e9128c17596ec2c0d370c21da0c1ce209
Varun Gowtham <v.gowtham@tu-berlin.de>
2019-05-07 11:05:18 +0200

    corrected mom

--- test_app.py
+++ test_app.py
@@ -6,58 +6,49 @@ from threading import Timer
 import json
 
 print "start of the script"
+
+def terminate():
+    print "STOP TEST"
+
+t = Timer(60.0, terminate)
+t.start()
+
 ems = os.environ["ET_EMS_LSBEATS_HOST"]
-# headers = {'content-type', 'text/plain'}
+headers = {'content-type': 'text/plain'}
 
-stampers = '''
-when e.strcmp(type,"net") do #NetData
-when e.path(message) do #TJobMsg
+stampers = "when e.tag(#testresult) do #websocket\n when e.tag(#terminate) do #websocket".encode()
 
-when e.tag(#testresulted) do #websocket
-'''
-
-moms = '''
-stream bool testcorrect = e.strmatch(message, "Hello Varun")
-
-trigger tjobfinished do emit testcorrect on #testresulted
-trigger tjobfinished do emit testcorrect on #websocket
-'''
-
+moms = "stream bool result := e.strmatch(message,\"STOP TEST\")\n trigger result do emit result on #terminate".encode()
 
 url = "http://" + ems + ":8888/stamper/tag0.1"
-# response = requests.post(url, headers=headers, data=stampers)
-response = requests.post(url, data=stampers)
+response = requests.post(url, headers=headers, data=stampers)
+#response = requests.post(url, data=stampers)
 print(response.content)
 
 url = "http://" + ems + ":8888/MonitoringMachine/signals0.1"
-# response = requests.post(url, headers=headers, data=moms)
-response = requests.post(url, data=moms)
+response = requests.post(url, headers=headers, data=moms)
+# # response = requests.post(url, data=moms)
 print(response.content)
+
+print "after sending requests"
 
 from websocket import create_connection
 url = "ws://" + ems + ":3232"
 ws = create_connection(url)
 i = 0
-#def terminate():
-#    print "Hello Varun"
 
-#t = Timer(60.0, terminate)
-#t.start()
+print "entering loop"
 
 while True:
   result = ws.recv()
   result = json.loads(result)
   i += 1
   if i==100:
-    print "Hello Varun"
+    print "STOP TEST"
 #  if "#websocket" in result["channels"]:
-#    print "websocket found"
-  if "#testresulted" in result["channels"]:
+#    print "websocket message found"
+  if "#terminate" in result["channels"]:
     print "test result found"
     break
-#  i+=1
-#  if i == 5:
-#    print "Hello Varun"
-#    break
 
-print "Hello Varun"
+print "STOP TEST"
