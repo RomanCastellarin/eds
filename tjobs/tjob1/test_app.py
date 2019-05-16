bc1384ad6e71932b7d299aa7357bb40c5e0f2b6a
Varun Gowtham <v.gowtham@tu-berlin.de>
2019-05-16 14:19:18 +0200

    first tjob

---
+++ tjobs/tjob1/test_app.py
@@ -0,0 +1,69 @@
+import requests
+import time
+import sys
+import os
+from threading import Timer
+import json
+from websocket import create_connection
+
+class MonitoringTest():
+  def __init__(self):
+    self.condition = True
+    self.ems = os.environ["ET_EMS_LSBEATS_HOST"]
+    self.headers = {'content-type': 'text/plain'}
+    self.stampers = ""
+    self.monMachines = ""
+    try:
+      self.duration = os.environ['EDS_TEST_DURATION']
+    except KeyError:
+      self.duration = 180
+    self.t = Timer(self.duration, self.terminate)
+    print("before sending requests")
+
+    # get the stampers from file
+    with open(os.environ['PWD'] + "/" + "stampers.txt") as f:
+      self.stampers = f.read()
+      f.close()
+
+    # send stampers to EMS
+    url = "http://" + ems + ":8888/stamper/tag0.1"
+    response = requests.post(url, headers=headers, data=stampers)
+    print(response.content)
+
+    # get the monitoring machines from the file
+    with open(os.environ['PWD'] + "/" + "monitoring_machines.txt") as f:
+      self.monMachines = f.read()
+
+    # send the monitoring machines to EMS
+    url = "http://" + ems + ":8888/MonitoringMachine/signals0.1"
+    response = requests.post(url, headers=headers, data=moms)
+    print(response.content)
+
+    print("after sending requests")
+    print("starting timer")
+    self.t.start()
+    print("entering loop function")
+    self.start_test()
+    print("exiting loop function")
+
+  def terminate(self):
+    self.condition = False
+    print("STOP_TEST")
+
+  def start_test(self):
+    url = "ws://" + ems + ":3232"
+    ws = create_connection(url)
+    print "entering loop"
+    while(self.condition):
+      result = ws.recv()
+      result = json.loads(result)
+      if "#terminate" in result["channels"]:
+        print result
+        print "test result found"
+        break
+
+if __name__ == "__main__":
+  print("Starting the test")
+  edstest = MonitoringTest()
+  print("Ending the test")
+
