5f7582db4d3253c734d9b79fb46de9b86cea42b6
Varun Gowtham <v.gowtham@tu-berlin.de>
2019-10-02 17:25:41 +0200

    enable self termination

--- sut/normal_sut/test_application.py
+++ sut/normal_sut/test_application.py
@@ -125,6 +125,8 @@ class TestApplication(XAE):
         gevent.spawn_later(120, self.app_shutdown)
 
     def app_shutdown(self):
+        json_message = {'ourmessage':'STOP_TEST'}
+        r = requests.post(self.hostport, json=json_message)
         os.kill(os.getpid(), signal.SIGTERM)
 
     def handle_actuator_out(self, cnt, con):
