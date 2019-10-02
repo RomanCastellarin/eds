59e4f00dba873acd7f3c3a8b802589ed1d45f916
Varun Gowtham <v.gowtham@tu-berlin.de>
2019-10-02 17:33:57 +0200

    satisfy result_2 condition

--- sut/normal_sut/test_application.py
+++ sut/normal_sut/test_application.py
@@ -125,7 +125,7 @@ class TestApplication(XAE):
         gevent.spawn_later(120, self.app_shutdown)
 
     def app_shutdown(self):
-        json_message = {'ourmessage':'STOP_TEST'}
+        json_message = {'ourmessage':'STOP_TEST', 'value':{'integer':10, 'low':9}}
         r = requests.post(self.hostport, json=json_message)
         os.kill(os.getpid(), signal.SIGTERM)
 
