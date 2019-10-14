b16a3b106e8069498c8aa15d5eb3743ef22dcade
Román Castellarin <romancastellarin@gmail.com>
2019-10-14 11:27:16 +0200

    debugging

--- sut/normal_sut2/__init__.py
+++ sut/normal_sut2/__init__.py
@@ -4,6 +4,6 @@ TODO: Add description here
 
 __version__ = "0.1"
 __description__ = "TestApplication (forked from Varun Gowtham's https://github.com/varungowtham/eds)"
-__author_name__ = "Román Castellarin"
+__author_name__ = "Roman Castellarin"
 __author_mail__ = "roman.castellarin@imdea.org"
 __requires__ = []
